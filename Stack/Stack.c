56f17c343124425997e5d0bccd58509175bf511b
Zir3a3l <31537900+YakovlIlya228@users.noreply.github.com>
2017-12-16 17:59:00 +0300

    Update Stack.c

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -1,51 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define OVERFLOW -101
-#define UNDERFLOW -102
-#define OUT_OF_MEMORY -103
-#define INIT_SIZE 20
-#define MULTIPLIER 2
-typedef int T;
 
-struct stack{
-   T *data;
-   int size, top;
-};
 
-void init(struct stack *st){
-    st ->top = NULL;
-    st = malloc(sizeof(st)); //выделение памяти
-    if(st == NULL){
-        exit(OUT_OF_MEMORY);
+int count = 0, top , size = 20;
+int stack[20];
+
+
+void Push(){
+    if(size==count){
+        printf("Stack is full!\n");
     }
-    st->size = INIT_SIZE;
-    st->data = malloc(st-> size * sizeof(T)); // выделение паямти под элементы стека
-    if (st->data = NULL){
-        free(st);
-        exit(OUT_OF_MEMORY);
-    }
-    st->top = 0;
-}
-
-void delete_stack(struct stack **st){
-    free((*st)->data); // освобождение памяти элементов стека
-    free(*st); // освобождение памяти
-    *st = NULL;
-}
-
-void resize(struct stack *st){
-    st->size *= MULTIPLIER; //увеличение кол-ва элментов стека в N раз
-    st->data = realloc(st->data, st->size * sizeof(T));
-    if (st->data == NULL){
-        exit(OVERFLOW);
+    else{
+        printf("Enter the value u want to add to stack:\n");
+        scanf("%i",&(top));
+        stack[count] = top;
+        printf("You added:%i to stack\n",stack[count]);
+        count++;
     }
 }
 
-void push(struct stack *st, T value){
+void Pop(){
+    if(count==0){
+        printf("Stack is empty!");
+    }
+    else{
+        stack[count-1] = 0;
+        count--;
+    }
+}
+
+void Resize(){
+    int add;
+    printf("Type in amount of items u want to add to stack:\n");
+    scanf("%i",&add);
+    size *= add;
+}
+
+void Check_Top(){
+    printf("%i",stack[count-1]);
+}
+
+void Check_Size(){
+    printf("%i",size);
+}
+void Check_Stack(){
+    if(count!=0){
+        int counter = 0;
+        for(counter;counter<count;counter++){
+            printf("Item %i:%i\n",counter,stack[counter]);
+        }
+    }
+    else{
+        printf("Stack is empty!\n");
+    }
+}
+
+int main(){
+    int act;
+    while(act!=7){
+        printf("\n\n\nActions to do with stack:\n1-Push\n2-Pop\n4-See top\n6-See all items of stack\n7-Exit\n:");
+        scanf("%i",&act);
+        if(act==1){
+            Push();
+        }
+        if(act==2){
+            Pop();
+        }
+//        if(act==3){
+//            Resize();
+//        }
+        if(act==4){
+            Check_Top();
+        }
+//        if(act==5){
+//            Check_Size();
+//        }
+        if(act==6){
+            Check_Stack();
+        }
+    }
+    exit(0);
 
 }
 
-int main() {
-
-    return 0;
-}
