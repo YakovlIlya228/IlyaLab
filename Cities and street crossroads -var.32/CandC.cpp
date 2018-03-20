3b65735da1f002aeff158b7b5f97cbe15a02c635
Zir3a3l <31537900+YakovlIlya228@users.noreply.github.com>
2018-03-20 15:40:14 +0300

    Update CandC.cpp

--- Cities and street crossroads -var.32/CandC.cpp
+++ Cities and street crossroads -var.32/CandC.cpp
@@ -12,8 +12,10 @@ void Cities ::delete_city(){
     CandC[city_i]=0;
     printf("City number %i was deleted!\n",city_i);
 }
-void Cities ::check() {
+void Operation ::check() {
+    Cities C;
     int n,num;
+    printf("%i, %i , %i",city_i,MAX_CROSSINGS, CandC[0]);
     for(n=1;n<city_i;n++){
         if(CandC[n]>MAX_CROSSINGS){
             MAX_CROSSINGS=CandC[n];
@@ -23,7 +25,7 @@ void Cities ::check() {
     printf("City with the most crossings is number %i with %i crossings!\n",num,MAX_CROSSINGS);
 }
 int main() {
-    Cities c;int tmp;bool check= true;
+    Cities c; Operation o;int tmp;bool check= true;
     printf("Type in number to choose action!\n1 - Add city\n2 - Delete city\n3 - Check for the city with most crossings among added\n4 - Exit\n:");
     while(check){
         cin >> tmp;
@@ -37,7 +39,7 @@ int main() {
                 c.delete_city();
                 continue;
             case 3:
-                c.check();
+                o.check();
                 continue;
             case 4:
                 exit(0);
