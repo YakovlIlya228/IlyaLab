3569136c05176a700116777f99737502cb0e986d
Zir3a3l <31537900+YakovlIlya228@users.noreply.github.com>
2018-03-20 15:42:36 +0300

    Update CandC.h

--- Cities and street crossroads -var.32/CandC.h
+++ Cities and street crossroads -var.32/CandC.h
@@ -1,18 +1,19 @@
-//
-// Created by Ilya Yakovlev on 01.03.2018.
-//
-
 #ifndef CITIES_AND_CROSSINGS_CANDC_H
 #define CITIES_AND_CROSSINGS_CANDC_H
 
 #include <iostream>
 using namespace std;
+int CandC[256], MAX_CROSSINGS=0, city_i=1;
+
 class Cities{
-    int CandC[256], MAX_CROSSINGS=0, city_i=1;
 public:
     Cities();
     void add_city(int crossings);
     void delete_city();
+};
+
+class Operation{
+public:
     void check();
 };
 #endif //CITIES_AND_CROSSINGS_CANDC_H
